3edb2ceaf5ff7556b3bdb4f8acabd00089fdb046
Zara Marks <zara.marks@reseau.eseo.fr>
2023-02-07 14:26:18 -0600

    5. Longest Palindromic Substring (MEDIUM)

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@ G
 src/PascalTriangle.java,3/c/3c5646fef969fde05fff30b2262c2d215d661c8b
 B
 src/Fibonacci.java,e/4/e4ca206f303f834f297ba289333914a272fe3780
+Y
+)src/DataTypes/LinkedList/LinkedlList.java,6/3/6308b1b217b688daa0a9e0b9c6284fe0c922f6fc
+N
+src/SortingAlgo/QuickSort.java,5/8/58d4d1269b444cc4089fd5d0dc09fa4304cbaef2
+X
+(src/Recursion/ExercisesMemorization.java,0/f/0f1573a043021f77fd39b09e5f5387f7482851cc
+W
+'src/DataTypes/Stacks/ExercisesEasy.java,e/a/ea995cda1370da0d4a5e4733d846572dd127cabc
+N
+src/SortingAlgo/MergeSort.java,5/a/5af481c08991a08b1bf63b1659aeaa8026c9a984
--- src/DataTypes/String/Exercises.java
+++ src/DataTypes/String/Exercises.java
@@ -43,4 +43,68 @@ public class Exercises {
 
         return -1;
     }
+
+    // 242. Valid Anagram EASY
+    // https://leetcode.com/problems/valid-anagram/description/
+    // Runtime 1ms beats 100%
+    // Memory 42mb beats 96%
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        int[] alphabet = new int[26];
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+
+        for(char sc: sChar){
+            alphabet[sc-'a'] +=1;
+        }
+        for(char ts: tChar){
+            alphabet[ts-'a'] -=1;
+        }
+
+        for(int i : alphabet) if(i!=0) return false;
+        return true;
+    }
+
+    // TODO 5. Longest Palindromic Substring (MEDIUM)
+    // https://leetcode.com/problems/longest-palindromic-substring/description/
+    //Runtime 3ms beats 99%
+    // Memory 41.9 beats 91%
+
+    public String longestPalindrome(String s) {
+
+        int sLength = s.length();
+
+        if(sLength== 1) return s;
+
+        char[] sArray = s.toCharArray();
+
+        if(sLength == 2 && sArray[0] == sArray[1]) return s;
+        else if (sLength == 2) return String.valueOf(sArray[0]);
+
+        int low = 0; int high = 0; int maxVal = 0; int lowS = 0;
+
+        for(int i = 0; i < sLength ; i ++){
+            high=i;
+            low=i;
+
+            while(high < sLength-1 && sArray[low] == sArray[high+1]){
+                high++;
+                i = high;
+            }
+
+            while(low > 0 && high < sLength-1 && sArray[low-1] == sArray[high+1] ){
+                low--;
+                high++;
+            }
+
+            if(high-low  > maxVal) {
+                lowS = low;
+                maxVal = high-low;
+            }
+        }
+        return s.substring(lowS, lowS+maxVal+1);
+
+    }
 }
