1af21b7a645267964c8c2b72f43fe84780f935e5
Zara Marks <zara.marks@reseau.eseo.fr>
2023-02-08 12:05:25 -0600

    greedy: 455. Assign Cookies EASY

--- src/DataTypes/Array/Greedy.java
+++ src/DataTypes/Array/Greedy.java
@@ -1,5 +1,7 @@
 package DataTypes.Array;
 
+import java.util.Arrays;
+
 public class Greedy {
 
     // TODO 55. Jump Game (medium)
@@ -25,5 +27,61 @@ public class Greedy {
 
         }
         return maxJump >= nums.length-1;
+    }
+
+    // TODO 860. Lemonade Change easy
+    // https://leetcode.com/problems/lemonade-change/description/
+    // Runtime 2ms beats 80 %
+    // Memory 53.7mb beats 71%
+
+    public boolean lemonadeChange(int[] bills) {
+
+        int c5 = 0;
+        int c10 = 0;
+
+        for(int i = 0; i < bills.length; i ++){
+
+            int bill = bills[i];
+            // if we get a five bill, we add it
+            if(bill == 5) c5++;
+            else if (c5 == 0) return false; // if its not and we do not have any FIVES left, we return false
+            else if(bill == 10) {c5--; c10++;} // if we get a TEN bill, we return the change
+            else if(bill== 20) { // Same for the TWENTY
+                if(c10 == 0) c5 -= 3;
+                else {c5--;c10--;}
+            }
+
+            if(c5 < 0 || c10 < 0 ) return false; // we check if we got any negative numbers
+        }
+
+        return c5>=0 && c10>=0;
+
+    }
+
+    // TODO 455. Assign Cookies EASY
+    // https://leetcode.com/problems/assign-cookies/description/
+    // Runtime 7 ms beast 100 %
+    // Mmeory 43mb beats 90 %
+    public int findContentChildren(int[] g, int[] s) {
+
+        int gPointer = 0;
+        int sPointer = 0;
+
+        int counter = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(gPointer < g.length && sPointer < s.length){
+
+            if(s[sPointer] >= g[gPointer]){
+                counter++;
+                gPointer++;
+            }
+            sPointer++;
+        }
+
+        return counter;
+
     }
 }
