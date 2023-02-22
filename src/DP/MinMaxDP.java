66b5c5c5950db8c32b386bbd47478d792e34c49f
Zara Marks <zara.marks@reseau.eseo.fr>
2023-02-22 14:29:40 -0600

    Ex 221 dp

--- src/DP/MinMaxDP.java
+++ src/DP/MinMaxDP.java
@@ -51,4 +51,51 @@ public class MinMaxDP {
         return grid[grid.length-1][grid[0].length-1];
 
     }
+    public int minPathSum2(int[][] grid) {
+
+        int height = grid.length;
+        int width = grid[0].length;
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+
+                if(row == 0 && col == 0) grid[row][col] = grid[row][col];
+                else if(row == 0 && col != 0) grid[row][col] = grid[row][col] + grid[row][col - 1];
+                else if(col == 0 && row != 0) grid[row][col] = grid[row][col] + grid[row - 1][col];
+                else grid[row][col] = grid[row][col] + Math.min(grid[row - 1][col], grid[row][col - 1]);
+            }
+        }
+        return grid[height - 1][width - 1];
+    }
+
+    // TODO 221. Maximal Square MEDIUM
+    // https://leetcode.com/problems/maximal-square/description/
+    // Runtime 6ms beats 63%
+    // Memory  66mb beats 11%
+
+    public int maximalSquare(char[][] matrix) {
+
+        int col = matrix[0].length;
+        int row = matrix.length;
+
+        if(col == 0 || row == 0 ) return 0;
+
+        int maxSize = 0;
+        int[][] dp = new int[row][col];
+
+        for(int i = 0; i < row; i ++){
+
+            for (int j = 0; j < col; j ++){
+                int val = matrix[i][j] -'0';
+                if(val == 0 ) continue; // if matrix is = 0 , we do not do anything
+
+                if(j == 0 || i == 0 )dp[i][j] = val; // if either j or i is 0. then we are at the "wall" of the matrix
+                else{
+                    dp[i][j] = Math.min( Math.min(dp[i][j-1],dp[i-1][j]) , dp[i-1][j-1]) + 1; // or else we look at the corner right
+                }
+                maxSize = Math.max(maxSize, dp[i][j]);
+            }
+        }
+
+        return maxSize*maxSize;
+    }
 }
